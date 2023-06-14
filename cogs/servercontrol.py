d372031127a61a671272bf6b00f621314d85ec32
Neil S <44575567+neilstottler@users.noreply.github.com>
2023-06-13 20:10:15 -0400

    removed unused bits

--- cogs/servercontrol.py
+++ cogs/servercontrol.py
@@ -18,20 +18,11 @@ config = global_config.sftp
 
 class Server(Cog):
 
-    #/server <server to do> <command>
-    #start, stop, restart
     @discord.slash_command(guild_ids=[global_config.bot_guild_ids])
     async def server(self, ctx, 
                      server: discord.Option(str, 'Select which server to interact with.', choices=global_config.servers, required=True), 
                      command: discord.Option(str, 'The command to send to the server.', choices=["start", "stop", "restart", "status"], required=True)):
         
-        servers = global_config.servers
-        commands = ["start", "stop", "restart"]
-
-        #inputs
-        # server, command
-        #                            \/ needs to define the service name since they are different
-        # systemctl --user <command> tf2-casual.service
         username = config[str(server)].username
         hostname = config[str(server)].hostname
         password = config[str(server)].password
@@ -48,8 +39,6 @@ class Server(Cog):
     
 #server connecting
 async def server_command(user, hostname, password, command, servicename, homedir):
-
-    commands = ["start", "stop", "restart", "update", "ping"]
 
     async with asyncssh.connect(
         hostname,
@@ -84,20 +73,13 @@ async def server_command(user, hostname, password, command, servicename, homedir
         #start server
         async def serverStartService(servicename, hostname):
             #check if it's running first
-            print(await serverStatusCheckerBool(servicename))
             if await serverStatusCheckerBool(servicename) is True:
-                return f":warning: The {servicename} on {hostname} is already running! :warning:"
-            else:
-                #starting the service
                 await conn.run(f'systemctl --user start {servicename}')
                 return f":white_check_mark: The {servicename} on {hostname} has been started. :white_check_mark:"
             
         #stop server
         async def serverStopService(servicename, hostname):
             if await serverStatusCheckerBool(servicename) is False:
-                return f":warning: The {servicename} on {hostname} is already stopped! :warning:"
-            else:
-                #stopping the service
                 await conn.run(f'systemctl --user stop {servicename}')
                 return f":octagonal_sign: The {servicename} on {hostname} has been stopped. :octagonal_sign:"
         
@@ -105,17 +87,10 @@ async def server_command(user, hostname, password, command, servicename, homedir
         async def serverRestartService(servicename, hostname):
             #is the service even there?
             if await serverStatusCheckerBool(servicename) is False:
-                return f":warning: The {servicename} on {hostname} is not running! :warning:"
-            else:
-                #restart the service
                 await conn.run(f'systemctl --user restart {servicename}')
                 return f":white_check_mark: The {servicename} on {hostname} has been restarted. :white_check_mark:"
             
 
-        #status - check status of service
-        #start - check if service is running first, if not start server, check if server is actually up
-        #stop - check if service is running, if it is, stop server, check that it actually stopped
-        #restart - check if servive is running, restart accordingly
         if command =='status':
             return await serverStatusChecker(servicename, hostname)
         if command == 'start':
